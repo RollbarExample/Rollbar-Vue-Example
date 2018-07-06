d648d0f95f34de523dd0c7f54a578bae8c5ee4fc
Wahid Ali <wahid.ali@psquickit.com>
2018-07-06 14:49:46 +0530

    Added source map

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://s3.us-east-2.amazonaws.com/rollbar-example/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
