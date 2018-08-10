f2c3ea40f05a2550458419374e9b6f83e4ae2bfd
RollbarExample <wahid.ali@psquickit.com>
2018-08-10 12:51:43 +0530

    Removed the parameter

--- src/main.js
+++ src/main.js
@@ -6,7 +6,17 @@ var Rollbar = require('vue-rollbar');
 Vue.use(Rollbar, {
       accessToken: 'da15d7f183f44701ae91244b38f5501d',
       captureUncaught: true,
+      captureUnhandledRejections: true,
+      enabled: true,
+      source_map_enabled: true,
       environment: 'production',
+      payload: {
+        client: {
+             javascript: {
+                code_version: '1.0'
+             }
+        }
+      }
 });
 Vue.config.errorHandler = function (err, vm, info) {
   console.error(err)
